15d015eccdfe36fdd31128edd93110f98094fbe8
ameelmohammad <77770908+ameelmohammad@users.noreply.github.com>
2023-11-03 12:41:55 +0530

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 echo "create ec2"
-aws ec2 terminate-instances --image-id ami-0287a05f0ef0e9d9a --instance-type t2.micro --key-name GIT --region ap-south-1
+aws ec2 run-instances --image-id ami-0287a05f0ef0e9d9a --instance-type t2.micro --key-name GIT --region ap-south-1
